9953afbb6513c94f5db822ce8ab5939d3537f38a
B-Lad <bhavin.lad27@gmail.com>
2026-03-10 21:49:53 +0400

    Delete lib directory

--- lib/uid.ts
+++
@@ -1,7 +0,0 @@
-
-"use client";
-
-// A simple utility for generating unique IDs with a given prefix.
-export function uid(prefix: string): string {
-  return `${prefix}-${Date.now().toString(36)}-${Math.random().toString(36).substring(2)}`;
-}
